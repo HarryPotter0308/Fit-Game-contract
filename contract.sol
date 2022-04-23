5afa234423ad518f5cdc55e1f848acf92f0599aa
HarryPotter0308 <dmitrydenisov56@gmail.com>
2022-04-24 08:42:08 +1000

    almost

--- contract.sol
+++ contract.sol
@@ -3,22 +3,26 @@
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.8.4;
 
 contract GAME is ERC721, Ownable {
-    mapping(address => uint256) private _Balances;
-    address private _Dev;
+
+    uint public constant MAX_SUPPLY = 7000;
+    uint public Price = 0.1 ether;
+    mapping(uint => uint256) private _CurrentTotalSupply;
+
+    address private _DevAddress;
     uint256 private _DevBalance;
-    uint256 public Price;
+    string private _MetaURI;
 
     struct MintHistory {
         address minter;
         uint256 tokenId;
     }
 
-    mapping(uint256 => address[]) private _Winners;
+    mapping(uint => address[]) private _Winners;
 
-    mapping(uint256 => MintHistory[]) private _Minted;
+    mapping(uint => MintHistory[]) private _Minted;
 
     mapping(uint256 => uint256) private _Rewards;
 
@@ -35,12 +39,29 @@ contract GAME is ERC721, Ownable {
         _;
     }
 
-    constructor(uint256 _price) ERC721("Fit In NFT", "FIT") {
+    constructor(uint256 _price, string memory _baseURI) ERC721("Fit In NFT", "FIT") {
         Price = _price;
+        _MetaURI = _baseURI;
     }
 
     function setPrice(uint256 _price) public onlyOwner {
         Price = _price;
+    }
+
+    function baseURI() internal view override returns (string memory) {
+        return _MetaURI;
+    }
+
+    function setBaseURI(string memory _baseURI) public onlyOwner {
+        _MetaURI = _baseURI;
+    }
+
+    function tokenURI(uint256 _tokenId) public override {
+        require(_tokenId > 0, "TokenID is invaliable");
+
+        if (_tokenId <= 1000) return string(abi.encodePacked(super.tokenURI(), ".json"));
+        else if (1000 < _tokenId && _tokenId < 5000) return string(abi.encodePacked(super.tokenURI(), ".json"));
+        else return string(abi.encodePacked(super.tokenURI(), ".json"));
     }
 
     function setWinners(uint256 version, uint256[] memory winners)
@@ -57,7 +78,7 @@ contract GAME is ERC721, Ownable {
         }
     }
 
-    function mint(string memory _tokenURI, uint256 _version)
+    function mint(uint256 _version)
         public
         payable
         _versionLimit(_version)
@@ -86,18 +107,32 @@ contract GAME is ERC721, Ownable {
     function distributeRewardsToWinners() public {
         uint256 index = 0;
 
-        payable(address(this)).transfer(_Dev, _Rewards[1] / _Minted[1].length);
+        payable(address(this)).transfer(_DevAddress, _DevBalance);
 
         while(_Minted[1][index].minter) {
             payable(address(this)).transfer(_Minted[1][index].minter, _Rewards[1] / _Minted[1].length);
+            index ++;
         }
         index = 0;
         while(_Minted[2][index].minter) {
             payable(address(this)).transfer(_Minted[2][index].minter, _Rewards[2] / _Minted[2].length);
+            index ++;
         }
         index = 0;
         while(_Minted[3][index].minter) {
             payable(address(this)).transfer(_Minted[3][index].minter, _Rewards[3] / _Minted[3].length);
+            index ++;
+        }
+    }
+
+    function claim() public {
+        uint256 index = 0;
+        uint256 _tokenId;
+        while (_Minted[1][index].minter) {
+            if (msg.sender == _Minted[1][index ++].minter) {
+                _tokenId = totalSupply() + 1;
+                _safeMint(msg.sender, _tokenId);
+            }
         }
     }
 }
